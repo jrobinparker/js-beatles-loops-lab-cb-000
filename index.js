553fe5c03b78620ea71a0113f1bc0f2b1c040f22
Jeremy Parker <jrobinparker@gmail.com>
2018-07-09 16:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var instruments = []
 
 function theBeatlesPlay(musicians, instruments) {
   var beatlesArray = []
-  for (var i = 1; i <= musicians.length; i ++) {
+  for (var i = 0; i < musicians.length; i ++) {
     beatlesArray.push("${musicians} plays ${instruments}")
   }
   return beatlesArray
